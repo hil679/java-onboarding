54e1a04f3f48d3676fb7dcb5bcf4dc493aacfd99
hil679 <hil679922@gmail.com>
2023-09-09 13:44:47 +0900

    Add: problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,14 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for(int i = 3; i <= number; i++){
+            answer += check369(String.valueOf(i));
+        }
         return answer;
     }
+    public static int check369(String integer){
+        String change369ToA = integer.replace("3", "").replace("6", "").replace("9", "");
+        return integer.length() - change369ToA.length();
+    }
+
 }
